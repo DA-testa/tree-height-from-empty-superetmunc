4accadb7c07a140d74c0a217903e9d5c86058a6a
Edmunds <93928858+superetmunc@users.noreply.github.com>
2023-03-05 15:30:49 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,44 +1,51 @@
+import os
 import sys
 import threading
 
 
 def compute_height(n, parents):
     nodes = [[] for _ in range(n)]
+    
     for i in range(n):
         if parents[i] == -1:
             root = i
         else:
             nodes[parents[i]].append(i)
+    
     def height(node):
         if not nodes[node]:
             return 1
         else:
             return 1 + max(height(child) for child in nodes[node])
+    
     return height(root)
 
 
 def main():
-    input_type = input("")
-    
+    input_type = input("Enter input type (I for keyboard, F for file): ")
     if input_type == "I":
         n = int(input())
         parents = list(map(int, input().split()))
+    
     elif input_type == "F":
-        filename = input("")
-        
+        filename = input("Enter filename: ")
+        if filename and 'a' not in filename:
+            path = os.path.join(os.getcwd(), 'test', filename)
         try:
-            with open(filename) as file:
+            with open(path) as file:
                 n = int(file.readline())
                 parents = list(map(int, file.readline().split()))
         except FileNotFoundError:
-            print("Error: File not found")
+            print(f"Error: File {filename} not found in test folder")
             return
     else:
-        print("Error: Invalid input type")
-        return
+        n = int(input())
+        parents = list(map(int, input().split()))
+    
     print(compute_height(n, parents))
 
 
-sys.setrecursionlimit(10**7)
+
+sys.setrecursionlimit(10**7) 
 threading.stack_size(2**27) 
 threading.Thread(target=main).start()
